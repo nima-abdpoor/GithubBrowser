6ac1b97dc1dccb0bc626902f841779b3db216a9c
nima abdpoor <nimaabdi328@yahoo.com>
2020-12-13 17:52:54 +0330

    ViewModel factory.

---
+++ di/src/main/java/com/nima/githubbrowser/di/viewmodel/AppViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.nima.githubbrowser.di.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+
+class AppViewModelFactory @Inject constructor(
+    private val classToViewModel :@JvmSuppressWildcards Map<Class<out ViewModel> , Provider<ViewModel>>
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST")
+        return classToViewModel[modelClass]?.get() as? T
+            ?:throw NullPointerException("No viewModel Mapping for class : ${modelClass.name}")
+    }
+}
---
+++ di/src/main/java/com/nima/githubbrowser/di/viewmodel/ViewModelKey.kt
@@ -0,0 +1,9 @@
+package com.nima.githubbrowser.di.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+
+@MapKey
+annotation class ViewModelKey(val classKey : KClass<out ViewModel>)

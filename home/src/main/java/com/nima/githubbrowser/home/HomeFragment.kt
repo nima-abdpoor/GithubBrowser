cf5ab11c5cd0c4555c8f0245fb19c4e73785c71e
nima abdpoor <nimaabdi328@yahoo.com>
2020-12-13 10:50:39 +0330

    home module and fragment for that completed.

---
+++ home/src/main/java/com/nima/githubbrowser/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.nima.githubbrowser.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nima.githubbrowser.home.databinding.ScreenHomeBinding
+
+
+class HomeFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding =ScreenHomeBinding.inflate(inflater,container , false)
+        return binding.root
+    }
+}
